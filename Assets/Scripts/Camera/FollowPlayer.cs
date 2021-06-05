e451625c69de4842fcba9ed03842567adbe60375
Alikhan <73962071@continental.edu.pe>
2021-06-05 14:22:44 -0500

    added camera follow player

---
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject mainCamera;
+    Vector3 offset = new Vector3(0,2,3);
+
+    void Start()
+    {
+        mainCamera=GameObject.Find("MainCamera");
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mainCamera.transform.position = player.transform.position + offset;
+
+    }
+}

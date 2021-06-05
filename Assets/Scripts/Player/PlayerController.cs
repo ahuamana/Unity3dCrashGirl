55bb64d70666de8d723757af703af24051c6d407
Alikhan <73962071@continental.edu.pe>
2021-06-05 13:25:51 -0500

    added forward and rotate player

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    float verticalInput;
+    float horizontalInput;
+    public float speedPlayer = 10;
+    public float speedPlayerRotation = 250;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //avanzar
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * speedPlayer * Time.deltaTime * verticalInput);
+
+        //rotar
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up * speedPlayerRotation * Time.deltaTime * horizontalInput);
+
+        if (verticalInput != 0)
+        { 
+            //animacion correr
+        
+        }
+
+    }
+}

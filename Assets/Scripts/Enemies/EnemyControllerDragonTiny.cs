f7bbd8a5ab399141a12b6657d877b1e5fb938cb1
Alikhan <73962071@continental.edu.pe>
2021-06-20 13:32:03 -0500

    collisions with enemy and items

---
+++ Assets/Scripts/Enemies/EnemyControllerDragonTiny.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControllerDragonTiny : MonoBehaviour
+{
+    Animator anim;
+    
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetTrigger("Attack 01");
+    }
+}
---
+++ Assets/Scripts/Player/Collisions/CollisionStar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionStar : MonoBehaviour
+{
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Star"))
+        {
+
+            Destroy(collision.gameObject);
+        
+        }    
+    }
+
+}

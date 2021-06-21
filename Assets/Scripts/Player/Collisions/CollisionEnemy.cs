208cabba32ffbe81b7676d568269c681b1237651
Alikhan <73962071@continental.edu.pe>
2021-06-20 23:26:04 -0500

    added time to wait for attacking player

---
+++ Assets/Scripts/Player/Collisions/CollisionEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionEnemy : MonoBehaviour
+{
+    public int cantidadDisminuir = 5;
+    public float tiempo;
+    public bool isAttacked = false;
+
+   
+
+    private void Update()
+    {
+        tiempo += Time.deltaTime;
+        
+
+    }
+
+    IEnumerator Hit()
+    {
+        //put how many seconds you want it to wait in WaitForSeconds(here)
+        yield return new WaitForSeconds(0.7f);
+        tiempo = 0;
+        isAttacked = false;
+        Debug.Log("timepo: "+ tiempo);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && tiempo >= 0.7f)
+        {
+            isAttacked = true;
+            StartCoroutine(Hit());
+        }
+    }
+
+
+}

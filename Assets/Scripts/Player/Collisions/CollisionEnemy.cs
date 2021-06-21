fef6e1d8ef40ad823b56c27883e7682b6752ae1a
Alikhan <73962071@continental.edu.pe>
2021-06-21 11:34:41 -0500

    added name cinematografic

--- Assets/Scripts/Player/Collisions/CollisionEnemy.cs
+++ Assets/Scripts/Player/Collisions/CollisionEnemy.cs
@@ -7,32 +7,48 @@ public class CollisionEnemy : MonoBehaviour
     public int cantidadDisminuir = 5;
     public float tiempo;
     public bool isAttacked = false;
+    public bool isInvulnerable = false;
+    Rigidbody rigidbody;
 
-   
+
+    private void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
 
     private void Update()
     {
         tiempo += Time.deltaTime;
-        
+
+        //Debug.Log("timepo: " + tiempo);
 
     }
+
+   
 
     IEnumerator Hit()
     {
         //put how many seconds you want it to wait in WaitForSeconds(here)
+        isAttacked = true;
+        //tiempo = 0;
         yield return new WaitForSeconds(0.7f);
-        tiempo = 0;
+        
+
         isAttacked = false;
-        Debug.Log("timepo: "+ tiempo);
+
 
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Enemy") && tiempo >= 0.7f)
+        if (collision.gameObject.CompareTag("Enemy"))
         {
-            isAttacked = true;
             StartCoroutine(Hit());
+            LivesManger.instance.disminuirVida(cantidadDisminuir);
+
+            //Retroceder
+            rigidbody.AddForce(Vector3.forward, ForceMode.Impulse);
         }
     }
 
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
 
     float verticalInput;
     float horizontalInput;
-    public float speedPlayer = 10;
+    public float speedPlayer = 5;
     public float speedPlayerRotation = 250;
     public float jumpSpeed = 200f;
 
@@ -27,15 +27,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //avanzar
-        verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.forward * speedPlayer * Time.deltaTime * verticalInput);
-        
+       
 
-
-        //rotar
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up * speedPlayerRotation * Time.deltaTime * horizontalInput);
 
         //Saltar
         if (Input.GetButtonDown("Jump") && isGrounded)
@@ -51,9 +44,40 @@ public class PlayerController : MonoBehaviour
         //Actualizar el animator y asignar la velocidad en cada frame
         anim.SetFloat("Speed", Mathf.Abs(verticalInput));
 
-        //
+        //Verificar si pisa el suelo o no
         isGrounded = Physics.Raycast(transform.position, Vector3.down, groundDistance);
-        
+
+
+        //Movimiento
+        ////avanzar
+        verticalInput = Input.GetAxis("Vertical");
+        //transform.Translate(Vector3.forward * speedPlayer * Time.deltaTime * verticalInput);
+
+
+        ////rotar
+        horizontalInput = Input.GetAxis("Horizontal");
+        //transform.Rotate(Vector3.up * speedPlayerRotation * Time.deltaTime * horizontalInput);
+
+
+
+        Vector3 monement = new Vector3(horizontalInput, 0, verticalInput).normalized;
+       
+
+        if (monement == Vector3.zero)
+        {
+            return;
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(monement);
+        Debug.Log(targetRotation.eulerAngles);
+        targetRotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 360 * Time.fixedDeltaTime);
+
+        ////Mover la position
+        rigidbody.MovePosition(rigidbody.position + monement * speedPlayer * Time.fixedDeltaTime);
+        ////Mover la rotacion
+        rigidbody.MoveRotation(targetRotation);
+
+
 
     }
 

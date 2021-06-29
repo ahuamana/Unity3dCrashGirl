0881d02fbcde309f110a6313e10c863f5ea45fc7
Alikhan <73962071@continental.edu.pe>
2021-06-28 19:40:54 -0500

    added some funtions to open portals

---
+++ Assets/Scripts/Enemies/EnemyFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public NavMeshAgent enemy;
+    public Transform player;
+
+    void Start()
+    {
+        enemy = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        enemy.SetDestination(player.position);
+    }
+
+}
---
+++ Assets/Scripts/Enemies/EnemyOrcBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyOrcBehavior : MonoBehaviour
+{
+
+    NavMeshAgent agent;
+    Animator anim;
+
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Speed", agent.speed);
+        //Debug.Log(agent.speed.ToString());
+    }
+}

709d0d677aa2c6fb2010ad485a374b7c58a0be06
Alikhan <73962071@continental.edu.pe>
2021-06-30 11:03:25 -0500

    added sound manager

---
+++ Assets/Scripts/AudioManager/SoundsManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundsManager : MonoBehaviour
+{
+
+    public static SoundsManager instance;
+
+    //music
+    public AudioClip music_background;
+
+    //Sound Effects
+    public AudioClip sfx_jump, sfx_money, sfx_flecha, sfx_Health, sfx_muerteFuego, sfx_attackEfecto;
+
+    //sound effect
+    public GameObject soundObject;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+   
+
+    void SoundObjectCreation(AudioClip clip)
+    {
+
+        //Create soundObject
+        GameObject newObject = Instantiate(soundObject, transform);
+        //Assign audioClip
+        newObject.GetComponent<AudioSource>().clip = clip;
+        //PlayAudio
+        newObject.GetComponent<AudioSource>().Play();
+
+    }
+
+
+    public void PlaySFX(string sfxname)
+    {
+        switch (sfxname)
+        {
+            case "jump":
+                SoundObjectCreation(sfx_jump);
+                break;
+
+            case "money":
+                SoundObjectCreation(sfx_money);
+                break;
+
+            case "flecha":
+                SoundObjectCreation(sfx_flecha);
+                break;
+
+            case "Health":
+                SoundObjectCreation(sfx_Health);
+                break;
+
+            case "muerteFuego":
+                SoundObjectCreation(sfx_muerteFuego);
+                break;
+
+            case "attackEfecto":
+                SoundObjectCreation(sfx_attackEfecto);
+                break;
+
+            case "background":
+                SoundObjectCreation(music_background);
+                break;
+
+            default:
+                Debug.Log("not sound found");
+                break;
+        }
+    }
+
+}
